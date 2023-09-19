36533ada050f0f55d0c7acadcb8699535e690e02
alwinsanil <alwinsanil@gmail.com>
2023-09-19 20:58:09 +0400

    second commit

---
+++ Day 3/tcpclient.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include<stdio.h>
+
+int main(void)
+{
+    int socket_desc;
+    struct sockaddr_in server_addr;
+    char server_message[65534], client_message[65534],name[30],pass[30];
+   
+    // Clean buffers:
+    memset(server_message,'\0',sizeof(server_message));
+    memset(client_message,'\0',sizeof(client_message));
+   
+    // Create socket:
+    socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+   
+    if(socket_desc < 0){
+        printf("Unable to create socket\n");
+        return -1;
+    }
+   
+    printf("Socket created successfully\n");
+    printf("\nUsername:");
+    gets(name);
+    printf("\nPassword:");
+    gets(pass);
+   
+    // Set port and IP the same as server-side:
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(65534);
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+   
+    // Send connection request to server:
+    if((strcmp(name,"admin")==0) && (strcmp(pass,"123")==0))
+    {
+   if(connect(socket_desc, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0)
+   {
+printf("Unable to connect\n");
+return -1;
+   }
+   printf("Connected with server successfully\n");
+    }
+    else
+    {
+    printf("Incorrect username or password...\nUnable to connect\n");
+       close(socket_desc);
+return -1;
+    }
+   
+    // Get input from the user:
+    printf("Enter message: ");
+    gets(client_message);
+   
+    // Send the message to server:
+    if(send(socket_desc, client_message, strlen(client_message), 0) < 0){
+        printf("Unable to send message\n");
+        return -1;
+    }
+   
+    // Receive the server's response:
+    if(recv(socket_desc, server_message, sizeof(server_message), 0) < 0){
+        printf("Error while receiving server's msg\n");
+        return -1;
+    }
+   
+    printf("Server's response: %s\n",server_message);
+   
+    // Close the socket:
+    close(socket_desc);
+   
+    return 0;
+}
+
